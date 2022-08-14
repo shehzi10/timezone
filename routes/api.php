318f412621b3fff022a13cf1df526ac5c2dfdfda
faraz_saeed <faraz.saeed@kaygees.com>
2022-08-14 21:20:46 +0500

    final update

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,8 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('clear', function () {
-    Artisan::call('route:clear');
-    Artisan::call('config:cache');
-    Artisan::call('config:clear');
     Artisan::call('cache:clear');
-    Artisan::call('config:cache');
-    Artisan::call('route:cache');
-    Artisan::call('optimize');
+    Artisan::call('config:clear');
     return apiresponse(true, "clear successfully");
 });
 
